88a3601e2820f8a37a31bf608bbf01577e1f9d2d
NivDantas <nivdantas@icloud.com>
2025-10-16 18:42:48 -0400

    Vercel JSON

--- src/app/components/MainContent.tsx
+++ src/app/components/MainContent.tsx
@@ -71,6 +71,7 @@ const MainContent = ({ children }: MainContentProps) => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 className="size-5 lg:size-6 xl:size-7 stroke-site-500 dark:stroke-neutral-400"
+
               >
                 <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                 <rect x="2" y="9" width="4" height="12"></rect>

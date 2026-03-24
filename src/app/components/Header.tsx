ba273ebbc9115289ce7cbb2738456c0126328515
NivDantas <dantasniv@gmail.com>
2026-03-23 22:31:16 -0300

    Add ScrollReveal animations; Update MainContent and Header for responsive design adjustments, finish desktop front-end layout.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,30 +4,31 @@ import {useTranslations} from "next-intl";
 const Header = () => {
     const t = useTranslations("Header");
     return (
-        <header className="grid grid-cols-1 justify-items-center mt-4 md:sticky z-50 top-0 text-site-800 dark:text-site-100">
-            <Image src={"n.svg"} alt={"N logo"} width={60} height={60} className="dark:invert"></Image>
+        <header className="grid grid-cols-1 justify-items-center mt-4 z-50 text-site-800 dark:text-site-100 md:h-dvh">
+            <Image src={"n.svg"} alt={"N logo"} width={60} height={60} className="dark:invert md:absolute md:left-10"></Image>
             <LanguageButton/>
-            <nav className="mx-10 mt-7.5">
+            <nav className="mx-10 mt-7.5 md:mt-4">
                 <ul className="flex gap-15 font-poppins">
                     <li>{t("navAbout")}</li>
                     <li>{t("navProjects")}</li>
                     <li>{t("navBlog")}</li>
                 </ul>
             </nav>
-            <h1 className="font-archivo font-black text-4xl tracking-tight text-center mt-10 leading-9">{t.rich("devTitle", {
+            <h1 className="font-archivo font-black md:text-7xl text-4xl tracking-tight md:tracking-normal text-center mt-10 md:mt-14 leading-9 md:leading-26">{t.rich("devTitle", {
                 br: () => <br />,
             })}</h1>
-            <nav className="mt-12.5 mb-12">
-                <ul className="flex gap-22">
+            <p className="hidden md:block md:mt-0 font-poppins text-xl leading-9 max-w-[50%] text-center">{t("devDesc")}</p>
+            <nav className="mt-12.5 mb-12 md:mb-0 md:h-min md:mt-4">
+                <ul className="flex gap-22 md:gap-11">
                     <li><a href="https://github.com/NivDantas">
-                        <Image src={"github.svg"} alt={"Github logo"} width={40} height={40} className="dark:invert"></Image>
-                    </a></li>
-                    <li><a href="mailto:nivdantas@icloud.com">
-                        <Image src={"envelope.svg"} alt={"Email logo"} width={40} height={40} className="dark:invert"></Image>
-                    </a></li>
-                    <li><a href="https://linkedin.com/in/nivaldo-dantas">
-                        <Image src={"linkedin.svg"} alt={"Linkedin logo"} width={40} height={40} className="dark:invert"></Image>
-                    </a></li>
+                            <Image src={"github.svg"} alt={"Github logo"} width={50} height={50} className="dark:invert w-10 h-10 md:w-[50px] md:h-[50px]"></Image>
+                        </a></li>
+                        <li><a href="mailto:nivdantas@icloud.com">
+                            <Image src={"envelope.svg"} alt={"Email logo"} width={50} height={50} className="dark:invert w-10 h-10 md:w-[50px] md:h-[50px]"></Image>
+                        </a></li>
+                        <li><a href="https://linkedin.com/in/nivaldo-dantas">
+                            <Image src={"linkedin.svg"} alt={"Linkedin logo"} width={50} height={50} className="dark:invert w-10 h-10 md:w-[50px] md:h-[50px]"></Image>
+                        </a></li>
                 </ul>
             </nav>
         </header>
--- src/app/components/MainContent.tsx
+++ src/app/components/MainContent.tsx
@@ -1,5 +1,6 @@
 import {useTranslations} from "next-intl";
 import React from "react";
+import ScrollReveal from "./ScrollReveal";
 
 interface MainContentProps {
     children?: React.ReactNode;
@@ -8,67 +9,98 @@ interface MainContentProps {
 const MainContent = ({children}: MainContentProps) => {
     const t = useTranslations("MainContent");
     return (
-        <main className="flex justify-center mx-10 text-site-800 dark:text-site-100">
+        <main className="flex justify-center ml-10 md:ml-30 md:mt-10 text-site-800 dark:text-site-100">
 
-            <div className="max-w-lg xl:max-w-xl flex flex-col justify-center">
-                <section>
-                    <h1 className="font-poppins font-medium text-[9px]">{t("experienceTitle")}</h1>
-                    <h2 className="font-poppins text-xs max-w-[24ch] mt-2.5">{t("experienceSubTitle")}</h2>
-                    <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-5 min-w-0 shrink-0">
-                       <div className="font-poppins">
-                           <h3 className="text-[8px] font-medium">{t("experienceMobileTitle")}</h3>
-                           <p className="text-[6px] mt-1">{t("experienceMobileParagraph")}</p>
-                       </div>
-                        <h3 className="font-poppins text-[7px] text-center font-medium">2024 - 2025</h3>
-                    </div>
-                    <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-5 min-w-0 shrink-0">
-                        <div className="font-poppins">
-                            <h3 className="text-[8px] font-medium">Freelancer</h3>
-                            <p className="text-[6px] mt-1">{t("experienceMobileParagraph2")}</p>
+            <div className="flex flex-col justify-center">
+                <ScrollReveal delay={200}>
+                    <section className="md:mr-90">
+                        <h1 className="font-poppins font-medium text-[9px] md:text-base">{t("experienceTitle")}</h1>
+                        <h2 className="font-poppins text-xs max-w-[24ch] mt-2.5 md:text-lg">{t("experienceSubTitle")}</h2>
+                        <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-5 min-w-0 shrink-0">
+                            <div className="font-poppins">
+                                <h3 className="text-[8px] md:text-sm font-medium">{t("experienceMobileTitle")}</h3>
+                                <p className="text-[6px] md:text-xs mt-1 text-site-700 dark:text-site-200">{t("experienceMobileParagraph")}</p>
+                            </div>
+                            <h3 className="font-poppins text-[7px] md:text-xs text-center font-medium">2024 -
+                                2025</h3>
                         </div>
-                        <h3 className="font-poppins text-[7px] text-center font-medium">2026 - Now</h3>
-                    </div>
+                        <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-5 min-w-0 shrink-0">
+                            <div className="font-poppins">
+                                <h3 className="text-[8px] md:text-sm font-medium">Freelancer</h3>
+                                <p className="text-[6px] mt-1 md:text-xs text-site-700 dark:text-site-200">{t("experienceMobileParagraph2")}</p>
+                            </div>
+                            <h3 className="font-poppins text-[7px] md:text-xs text-center font-medium">2026 - Now</h3>
+                        </div>
 
-                </section>
-                <section className="mt-5">
-                    <h1 className="font-poppins font-medium text-[9px]">{t("educationTitle")}</h1>
-                    <h2 className="font-poppins text-xs max-w-[24ch] mt-2.5">{t("educationSubTitle")}</h2>
-                    <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-5 min-w-0 shrink-0">
-                        <div className="font-poppins">
-                            <h3 className="text-[8px] font-medium">{t("educationMobileTitle")}</h3>
-                            <p className="text-[6px] mt-1">{t("educationMobileParagraph")}</p>
-                        </div>
-                        <h3 className="font-poppins text-[7px] text-center font-medium">2020 - 2025</h3>
-                    </div>
-                    <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-5 min-w-0 shrink-0">
-                        <div className="font-poppins">
-                            <h3 className="text-[8px] font-medium">{t("papeTitle")}</h3>
-                            <p className="text-[6px] mt-1">{t("papeParagraph")}</p>
-                        </div>
-                        <h3 className="font-poppins text-[7px] text-center font-medium">2021 - 2024</h3>
-                    </div>
-                </section>
+                    </section>
 
-                <section className="mt-10">
-                    <h1 className="font-poppins font-medium text-[9px]">Portfolio</h1>
-                    <h2 className="font-poppins text-xs max-w-[24ch] mt-2.5">{t("portfolioSubTitle")}</h2>
-                    <div id="PLACEHOLDER" className="w-[295px] h-[94px] bg-gray-500 mt-4 rounded-[10px]"></div>
-                    <div id="PLACEHOLDER" className="w-[295px] h-[94px] bg-gray-500 mt-5 rounded-[10px]"></div>
-                    <div id="PLACEHOLDER" className="w-[295px] h-[94px] bg-gray-500 mt-5 rounded-[10px]"></div>
-                </section>
-
-                <section className="mt-20">
-                    <h1 className="font-poppins font-medium text-[9px]">Blogspot</h1>
-                    <h2 className="font-poppins text-xs max-w-[24ch] mt-2.5">{t("blogSubTitle")}</h2>
-                    <article id="PLACEHOLDER" className="w-[288px] h-[156px] bg-gray-800 mt-3 rounded-2xl"></article>
-                    <article id="PLACEHOLDER" className="grid grid-cols-[1fr_4fr] mt-7 gap-3">
-                       <div id="PLACEHOLDER" className="w-[73px] h-[53px] bg-gray-800 rounded-md"></div>
-                        <div className="flex flex-col gap-2 justify-center">
-                            <h1 className="font-poppins text-[8px]">Title</h1>
-                            <h2 className="font-poppins text-[8px]">Niv - Data</h2>
+                    <section className="mt-5 md:mr-90">
+                        <h1 className="font-poppins font-medium text-[9px] md:text-base">{t("educationTitle")}</h1>
+                        <h2 className="font-poppins text-xs max-w-[24ch] mt-2.5 md:text-lg">{t("educationSubTitle")}</h2>
+                        <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-5 min-w-0 shrink-0">
+                            <div className="font-poppins">
+                                <h3 className="text-[8px] md:text-sm font-medium">{t("educationMobileTitle")}</h3>
+                                <p className="text-[6px] mt-1 md:text-xs text-site-700 dark:text-site-200">{t("educationMobileParagraph")}</p>
+                            </div>
+                            <h3 className="font-poppins text-[7px] md:text-xs text-center font-medium">2020 - 2025</h3>
                         </div>
-                    </article>
-                </section>
+                        <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-5 min-w-0 shrink-0">
+                            <div className="font-poppins">
+                                <h3 className="text-[8px] md:text-sm font-medium">{t("papeTitle")}</h3>
+                                <p className="text-[6px] md:text-xs text-site-700 dark:text-site-200 mt-1">{t("papeParagraph")}</p>
+                            </div>
+                            <h3 className="font-poppins text-[7px] md:text-xs text-center font-medium">2021 - 2024</h3>
+                        </div>
+                    </section>
+                </ScrollReveal>
+
+                <ScrollReveal delay={200}>
+                    <section className="mt-10 md:mt-25">
+                        <h1 className="font-poppins font-medium text-[9px] md:text-base">Portfolio</h1>
+                        <h2 className="font-poppins text-xs max-w-[24ch] mt-2.5 md:text-lg">{t("portfolioSubTitle")}</h2>
+                        <article className="md:flex md:flex-wrap md:gap-4">
+                        <div id="PLACEHOLDER" className="w-[295px] h-[94px] md:w-[320px] md:h-[240px] bg-gray-500 mt-4 md:mt-5 rounded-[10px]"></div>
+                        <div id="PLACEHOLDER" className="w-[295px] h-[94px] md:w-[320px] md:h-[240px] bg-gray-500 mt-5 rounded-[10px]"></div>
+                        <div id="PLACEHOLDER" className="w-[295px] h-[94px] md:w-[320px] md:h-[240px] bg-gray-500 mt-5 rounded-[10px]"></div>
+                        <div id="PLACEHOLDER" className="hidden md:block w-[320px] h-[240px] bg-gray-500 mt-5 rounded-[10px]"></div>
+                        <div id="PLACEHOLDER" className="hidden md:block w-[320px] h-[240px] bg-gray-500 mt-5 rounded-[10px]"></div>
+                        <div id="PLACEHOLDER" className="hidden md:block w-[320px] h-[240px] bg-gray-500 mt-5 rounded-[10px]"></div>
+                        </article>
+                    </section>
+                </ScrollReveal>
+
+                <ScrollReveal delay={200}>
+                    <section className="mt-10 md:mt-25">
+                        <h1 className="font-poppins font-medium text-[9px] md:text-base">Blogspot</h1>
+                        <h2 className="font-poppins text-xs max-w-[24ch] mt-2.5 md:text-lg">{t("blogSubTitle")}</h2>
+                        <article className="md:grid md:grid-cols-[1fr_1fr] md:gap-4">
+                        <div id="PLACEHOLDER"
+                                 className="w-[288px] h-[156px] md:w-[692px] md:h-[375px] bg-gray-800 mt-3 rounded-2xl">
+                        </div>
+                        <div className="hidden md:flex flex-col gap-2 justify-center">
+                            <h1 className="font-poppins text-[8px] md:text-xl">Title</h1>
+                            <h2 className="font-poppins text-[8px] md:text-xl">Niv - Data</h2>
+                        </div>
+                        </article>
+                        <article id="PLACEHOLDER" className="grid grid-cols-[1fr_4fr] md:grid-cols-[175px_1fr] mt-7 gap-3 md:gap-8">
+                            <div id="PLACEHOLDER" className="w-[73px] md:w-[175px] h-[53px] md:h-[125px] bg-gray-800 rounded-md"></div>
+                            <div className="flex flex-col gap-2 justify-center">
+                                <h1 className="font-poppins text-[8px] md:text-base">Title</h1>
+                                <h2 className="font-poppins text-[8px] md:text-base">Niv - Data</h2>
+                            </div>
+                            <div id="PLACEHOLDER" className="hidden md:block w-[73px] md:w-[175px] h-[53px] md:h-[125px] bg-gray-800 rounded-md"></div>
+                            <div className="hidden md:flex flex-col gap-2 justify-center">
+                                <h1 className="font-poppins text-[8px] md:text-base">Title</h1>
+                                <h2 className="font-poppins text-[8px] md:text-base">Niv - Data</h2>
+                            </div>
+                            <div id="PLACEHOLDER" className="hidden md:block w-[73px] md:w-[175px] h-[53px] md:h-[125px] bg-gray-800 rounded-md"></div>
+                            <div className="hidden md:flex flex-col gap-2 justify-center">
+                                <h1 className="font-poppins text-[8px] md:text-base">Title</h1>
+                                <h2 className="font-poppins text-[8px] md:text-base">Niv - Data</h2>
+                            </div>
+                        </article>
+                    </section>
+                </ScrollReveal>
             </div>
         </main>
     );

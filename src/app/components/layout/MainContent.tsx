548991a8690425be7f71ba08dfe119b2f3dc9dee
NivDantas <dantasniv@gmail.com>
2026-04-08 03:18:57 -0300

    Change description of PAPE experience.

--- src/app/components/layout/MainContent.tsx
+++ src/app/components/layout/MainContent.tsx
@@ -17,6 +17,15 @@ const MainContent = ({portfolioContent, blogContent}: MainContentProps) => {
                     <section className="md:mr-90 2xl:mr-15 lg:mr-5 xl:mb-13">
                         <h1 className="font-poppins font-medium text-[9px] xl:text-xl md:text-base xl:mb-6">{t("experienceTitle")}</h1>
                         <h2 className="font-poppins text-xs max-w-[24ch] mt-2.5 md:text-lg xl:text-3xl xl:mb-13">{t("experienceSubTitle")}</h2>
+                        <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-6 min-w-0 shrink-0">
+                            <div className="font-poppins">
+                                <h3 className="text-[8px] md:text-sm font-medium xl:text-2xl xl:mb-3">{t("papeTitle")}</h3>
+                                <p className="text-[6px] md:text-xs text-site-700 dark:text-site-200 mt-1 xl:text-base">{t("papeParagraph")}</p>
+                            </div>
+                            <h3 className="font-poppins text-[7px] md:text-xs text-center font-medium xl:text-lg">2021 -
+                                2024</h3>
+                        </div>
+
                         <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-5 min-w-0 shrink-0">
                             <div className="font-poppins">
                                 <h3 className="text-[8px] xl:text-2xl md:text-sm font-medium xl:mb-3">{t("experienceMobileTitle")}</h3>
@@ -25,12 +34,14 @@ const MainContent = ({portfolioContent, blogContent}: MainContentProps) => {
                             <h3 className="font-poppins text-[7px] md:text-xs xl:text-lg text-center font-medium">2024 -
                                 2025</h3>
                         </div>
+
                         <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-6 min-w-0 shrink-0">
                             <div className="font-poppins">
                                 <h3 className="text-[8px] md:text-sm xl:text-2xl font-medium xl:mb-3">Freelancer</h3>
                                 <p className="text-[6px] mt-1 md:text-xs xl:text-base text-site-700 dark:text-site-200">{t("experienceMobileParagraph2")}</p>
                             </div>
-                            <h3 className="font-poppins text-[7px] md:text-xs xl:text-lg text-center font-medium">2026 - Now</h3>
+                            <h3 className="font-poppins text-[7px] md:text-xs xl:text-lg text-center font-medium">2026 -
+                                Now</h3>
                         </div>
 
                     </section>
@@ -43,15 +54,10 @@ const MainContent = ({portfolioContent, blogContent}: MainContentProps) => {
                                 <h3 className="text-[8px] md:text-sm font-medium xl:text-2xl xl:mb-3">{t("educationMobileTitle")}</h3>
                                 <p className="text-[6px] mt-1 md:text-xs text-site-700 xl:text-base dark:text-site-200">{t("educationMobileParagraph")}</p>
                             </div>
-                            <h3 className="font-poppins text-[7px] md:text-xs text-center font-medium xl:text-lg">2020 - 2025</h3>
+                            <h3 className="font-poppins text-[7px] md:text-xs text-center font-medium xl:text-lg">2020 -
+                                2025</h3>
                         </div>
-                        <div className="grid grid-cols-[2.5fr_1fr] gap-6 mt-6 min-w-0 shrink-0">
-                            <div className="font-poppins">
-                                <h3 className="text-[8px] md:text-sm font-medium xl:text-2xl xl:mb-3">{t("papeTitle")}</h3>
-                                <p className="text-[6px] md:text-xs text-site-700 dark:text-site-200 mt-1 xl:text-base">{t("papeParagraph")}</p>
-                            </div>
-                            <h3 className="font-poppins text-[7px] md:text-xs text-center font-medium xl:text-lg">2021 - 2024</h3>
-                        </div>
+
                     </section>
                 </ScrollReveal>
 
@@ -60,8 +66,9 @@ const MainContent = ({portfolioContent, blogContent}: MainContentProps) => {
                         <h1 className="font-poppins font-medium text-[9px] md:text-base xl:text-xl xl:mb-6">Portfolio</h1>
                         <h2 className="font-poppins text-xs max-w-[24ch] mt-2.5 md:text-lg xl:text-3xl xl:mb-14">{t("portfolioSubTitle")}</h2>
 
-                        <article className="md:grid xl:grid xl:grid-cols-3 md:grid-cols-2 md:gap-4 mt-4 mr-30 justify-items-stretch">
-                                {portfolioContent}
+                        <article
+                            className="md:grid xl:grid xl:grid-cols-3 md:grid-cols-2 md:gap-4 mt-4 mr-30 justify-items-stretch">
+                            {portfolioContent}
                         </article>
                     </section>
                 </ScrollReveal>
